f9254accca1e8589cd102f10e593c4973b7c454e
Ubugeeei <ubuge1122@gamil.com>
2022-07-24 15:10:25 +0900

    struct method

--- src/main.zig
+++ src/main.zig
@@ -236,3 +236,19 @@ test "struct usage" {
     };
     _ = my_vector;
 }
+
+const Stuff = struct {
+    x: i32,
+    y: i32,
+    fn swap(self: *Stuff) void {
+        const tmp = self.x;
+        self.x = self.y;
+        self.y = tmp;
+    }
+};
+test "automatic dereference" {
+    var thing = Stuff{ .x = 10, .y = 20 };
+    thing.swap();
+    try assert(thing.x, 20);
+    try assert(thing.y, 10);
+}
