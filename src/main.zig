92a1a265864e0baad2123a5d9569a306e6d03293
Ubugeeei <ubuge1122@gamil.com>
2022-07-24 15:24:03 +0900

    vector

--- src/main.zig
+++ src/main.zig
@@ -273,3 +273,11 @@ test "optional" {
     }
     try assert(found_index, null);
 }
+
+test "vector add" {
+    const Vector = @import("std").meta.Vector;
+    const x: Vector(4, f32) = .{ 1, -10, 20, -1 };
+    const y: Vector(4, f32) = .{ 2, 10, 0, 1 };
+    const z = x + y;
+    try assert(z, Vector(4, f32){ 3, 0, 20, 0 });
+}
