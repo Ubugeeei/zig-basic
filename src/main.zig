b34419256e299a740bb0e2e263b66ae3d62afa53
Ubugeeei <ubuge1122@gamil.com>
2022-07-24 14:54:41 +0900

    switch statement

--- src/main.zig
+++ src/main.zig
@@ -125,3 +125,19 @@ test "exception" {
         return;
     };
 }
+
+test "switch statement" {
+    var x: i8 = 10;
+    switch (x) {
+        -1...1 => {
+            x = -x;
+        },
+        10, 100 => {
+            //special considerations must be made
+            //when dividing signed integers
+            x = @divExact(x, 10);
+        },
+        else => {},
+    }
+    try assert(x, 1);
+}
