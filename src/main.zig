953e1835c786f872d34cb4052fd14a36fff2a317
Ubugeeei <ubuge1122@gamil.com>
2022-07-24 14:48:57 +0900

    for

--- src/main.zig
+++ src/main.zig
@@ -86,3 +86,23 @@ test "while with break" {
     }
     try assert(sum, 1);
 }
+
+test "for" {
+    //character literals are equivalent to integer literals
+    const string = [_]u8{ 'a', 'b', 'c' };
+
+    for (string) |character, index| {
+        _ = character;
+        _ = index;
+    }
+
+    for (string) |character| {
+        _ = character;
+    }
+
+    for (string) |_, index| {
+        _ = index;
+    }
+
+    for (string) |_| {}
+}
