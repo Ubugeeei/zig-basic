adb71e4340b905a31ab7fd237ad1e87a1424807a
Ubugeeei <ubuge1122@gamil.com>
2022-07-24 14:36:45 +0900

    vars

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,19 @@ const assert = std.testing.expectEqual;
 pub fn main() anyerror!void {}
 
 test "basic test" {
-    {
-        try assert(10, 3 + 7);
-    }
+    try assert(10, 3 + 7);
+}
+
+test "vars" {
+    const a: i32 = 3;
+    var b: i32 = 1;
+    b = 7;
+    try assert(a + b, 10);
+}
+
+test "assertion" {
+    var x: i32 = 0;
+
+    x = @as(i32, 1);
+    try assert(x, 1);
 }
